fb078a92711fa72dd087f3acdfb16993802a20e9
Jéssica Florentino <jdgflorentino@gmail.com>
2022-03-23 09:47:41 -0300

    Requisito 03 - lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
