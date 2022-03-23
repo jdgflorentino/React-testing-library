2f5109f857aeab677ff7b46b5cd9f2dd78bf8522
Jéssica Florentino <jdgflorentino@gmail.com>
2022-03-22 21:05:06 -0300

    Requisito 02.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const infoPokedex = screen.getByText(/this application simulates a Pokédex/i);
+    const infoPokemons = screen.getByText(/one can filter Pokémons/i);
+    expect(infoPokedex).toBeDefined();
+    expect(infoPokemons).toBeDefined();
+  });
+
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const headingAbout = screen.getByRole('heading',
+      { level: 2, name: /about pokédex/i });
+    expect(headingAbout).toBeDefined();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraphs = screen.getAllByText(/pokémons/i);
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByAltText(/pokédex/i);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(url);
+  });
+});
